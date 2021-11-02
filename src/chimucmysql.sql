0e02f8a9211bd33e4ca5ed023ffc7fab3053f3ba
Your Name <you@example.com>
2021-11-02 10:59:11 +0700

    Stored Prucedure

--- src/chimucmysql.sql
+++ src/chimucmysql.sql
@@ -1,8 +1,25 @@
 use classicmodels;
-select * from customers where customerName = '%Land of Toys Inc.%';
-explain select * from customers where customerName = 'Land of Toys Inc.';
-alter table customers add index idx_customerName(customerName);
-explain select * from customers where customerName = 'Land of Toys Inc.';
-alter table customers add index idx_full_name(contactFirstName, contactLastName);
-explain select * from customers where contactFirstName = 'Jean' or contactFirstName = 'King';
-alter table customers drop index idx_full_name;
+DELIMITER //
+CREATE PROCEDURE findAllCustomers()
+BEGIN
+    SELECT * FROM customers;
+END //
+
+DELIMITER ;
+call findAllCustomers();
+
+
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS `findAllCustomers`//
+
+CREATE PROCEDURE findAllCustomers()
+
+BEGIN
+
+    SELECT * FROM customers where customerNumber = 177;
+
+END //
+DELIMITER ;
+
+call findAllCustomers();
